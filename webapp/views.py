a2f33d4e83004daa4e32b65bb4f6ad209fc0d30e
flohms18 <trouilletflorent@outlook.com>
2026-08-15 17:45:50 +0200

    Changed many view

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.order_by('-published_date')
+    paginator = Paginator(posts, 5)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    return render(request, 'home.html', {'page_obj': page_obj})
+
+
+def article(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'article.html', {'post': post})
